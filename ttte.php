34139fc17270aaf2a14ba9419bd815dd64bc3f75
lindseyemaddox <lindseyemaddox@gmail.com>
2015-05-14 00:54:21 -0500

    some tie page

---
+++ ttte.php
@@ -0,0 +1,40 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/head.php'); // HTTP head?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/navigation.php'); //navigation?>
+
+<section id="mast">
+	
+	<img src="/_assets/img/header-ttte.jpg" alt="current Tennessee Tech Tuba Ensemble photo" title="current Tennessee Tech Tuba Ensemble photo">
+
+</section><!--mast-->
+
+<section id="main" class="morris">
+
+	<div class="inner">
+	
+		<h1>The Tennessee Tech Tuba Ensemble’s 48th season</h1>
+			
+		<h2>Euphoniums:</h2>
+
+		<h3>Guy Bortz, <span>Kingsport, TN,</span></h3>
+		<h3>Mary Tallman, <span>Bristol, VA,</span></h3>
+		<h3>Cody Dailey, <span>Murfreesboro, TN,</span></h3>
+		<h3>Tyler Thurber, <span>Shelbyville, TN,</span></h3>
+		<h3>Michael Engelhart, <span>Sweetwater, TN,</span></h3>
+		<h3>Jordan Watkins, <span>Bolivar, TN,</span></h3>
+		<h3>Justin Gorodetzky, <span>White House, TN,</span></h3>
+		<h3>Amanda Werlein, <span>Tullahoma, TN,</span></h3>
+		<h3>Wimberly Hall, <span>Hixson, TN,</span></h3>
+		<h3>Robert Wormsley, <span>Signal Mountain, TN,</span></h3>
+		<h3>Adam Mullican, <span>Hermitage, TN</span></h3>
+
+		<h2 class="multiple">Tubas:</h2>
+
+		<h3>Austin Cooper, <span>Mt. Juliet, TN,</span></h3>
+		John Paul Powers, Clinton, TN, Caleb Davidson, Tullahoma, TN, Travis Roberson, Church Hill, TN, Ashanti Demas, Locus Grove, VA, Ryan Snell, Charleston, SC, Nolan Derrick, Church Hill, TN, Jalen Talley, Huntsville, AL, Curtiss Hoeppner, Lake Charles, LA, Quinton Webber, Clinton, TN, Josh Maberry, Oak Ridge, TN, Ottie West, Smyrna, TN, Lincoln Myers, Tullahoma, TN, Aaron Whitfield, Blountville, TN</h3>
+
+
+	</div><!--inner-->
+
+</section><!--main-->
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/footer.php'); // footer, close body and html?>
