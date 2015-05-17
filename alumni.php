1e6666fac90a6dca9db7d00011c01c713abe1953
lindseyemaddox <lindseyemaddox@gmail.com>
2015-05-16 22:58:21 -0500

    alumni page setup done pre-db

---
+++ alumni.php
@@ -0,0 +1,322 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/head.php'); // HTTP head?>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+<script src="/_assets/js/isotope.jquery.min.js"></script>
+<script src="/_assets/js/fancybox.jquery.js"></script>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/navigation.php'); //navigation?>
+
+<section id="mast">
+	
+	<img src="/_assets/img/header-alumni.jpg" alt="vintage Tennessee Tech Tuba Ensemble photo" title="vintage Tennessee Tech Tuba Ensemble photo">
+
+</section><!--mast-->
+
+<section id="main" class="alumni">
+
+	<div class="inner">
+	
+		<h1>The Tennessee Tech Tuba Ensemble Alumni</h1>
+		
+    	<div id="slider-cont">
+
+      		<div id="options" class="clearfix">
+
+        		<ul id="filters" class="option-set clearfix" data-option-key="filter">
+          			<li><a href="#filter" data-option-value=".y1967">1967</a></li>
+          			<li><a href="#filter" data-option-value=".y1968">1968</a></li>
+          			<li><a href="#filter" data-option-value=".y1969">1969</a></li>
+          			<li><a href="#filter" data-option-value=".y1970">1970</a></li>
+          			<li><a href="#filter" data-option-value=".y1971">1971</a></li>
+          			<li><a href="#filter" data-option-value=".y1972">1972</a></li>
+          			<li><a href="#filter" data-option-value=".y1973">1973</a></li>
+          			<li><a href="#filter" data-option-value=".y1974">1974</a></li>
+          			<li><a href="#filter" data-option-value=".y1975">1975</a></li>
+          			<li><a href="#filter" data-option-value=".y1976">1976</a></li>
+          			<li><a href="#filter" data-option-value=".y1977">1977</a></li>
+          			<li><a href="#filter" data-option-value=".y1978">1978</a></li>
+          			<li><a href="#filter" data-option-value=".y1979">1979</a></li>
+          			<li><a href="#filter" data-option-value=".y1980">1980</a></li>
+          			<li><a href="#filter" data-option-value=".y1981">1981</a></li>
+          			<li><a href="#filter" data-option-value=".y1982">1982</a></li>
+          			<li><a href="#filter" data-option-value=".y1983">1983</a></li>
+          			<li><a href="#filter" data-option-value=".y1984">1984</a></li>
+          			<li><a href="#filter" data-option-value=".y1985">1985</a></li>
+          			<li><a href="#filter" data-option-value=".y1986">1986</a></li>
+          			<li><a href="#filter" data-option-value=".y1987">1987</a></li>
+          			<li><a href="#filter" data-option-value=".y1988">1988</a></li>
+          			<li><a href="#filter" data-option-value=".y1989">1989</a></li>
+          			<li><a href="#filter" data-option-value=".y1990">1990</a></li>
+          			<li><a href="#filter" data-option-value=".y1991">1991</a></li>
+          			<li><a href="#filter" data-option-value=".y1992">1992</a></li>
+          			<li><a href="#filter" data-option-value=".y1993">1993</a></li>
+          			<li><a href="#filter" data-option-value=".y1994">1994</a></li>
+          			<li><a href="#filter" data-option-value=".y1995">1995</a></li>
+          			<li><a href="#filter" data-option-value=".y1996">1996</a></li>
+          			<li><a href="#filter" data-option-value=".y1997">1997</a></li>
+          			<li><a href="#filter" data-option-value=".y1998">1998</a></li>
+          			<li><a href="#filter" data-option-value=".y1999">1999</a></li>
+          			<li><a href="#filter" data-option-value=".y2000">2000</a></li>
+          			<li><a href="#filter" data-option-value=".y2001">2001</a></li>
+          			<li><a href="#filter" data-option-value=".y2002">2002</a></li>
+          			<li><a href="#filter" data-option-value=".y2003">2003</a></li>
+          			<li><a href="#filter" data-option-value=".y2004">2004</a></li>
+          			<li><a href="#filter" data-option-value=".y2005">2005</a></li>
+          			<li><a href="#filter" data-option-value=".y2006">2006</a></li>
+          			<li><a href="#filter" data-option-value=".y2007">2007</a></li>
+          			<li><a href="#filter" data-option-value=".y2008">2008</a></li>
+          			<li><a href="#filter" data-option-value=".y2009">2009</a></li>
+          			<li><a href="#filter" data-option-value=".y2010">2010</a></li>
+          			<li><a href="#filter" data-option-value=".y2011">2011</a></li>
+          			<li><a href="#filter" data-option-value=".y2012">2012</a></li>
+          			<li><a href="#filter" data-option-value=".y2013">2013</a></li>
+          			<li><a href="#filter" data-option-value=".y2014">2014</a></li>
+          			<li><a href="#filter" data-option-value=".y2015">2015</a></li>
+        		</ul>
+
+      		</div><!--options-->
+
+      		<div class="slider-row">
+
+	      		<h4>1967</h4>
+
+	        	<div class="slider"></div>
+
+	      		<h4>2015</h4>
+
+	      	</div><!--slider-row-->
+
+      		<div class="clearfix"></div>
+
+    	</div><!--slider-cont-->
+
+		<div class="wrapper">
+
+		    <div class="main">
+
+		        <div class="container">
+			
+			    	<aside class="sorter">
+			    		
+			      		<div id="sidebar-options" class="clearfix">
+
+			        		<ul id="sidebar-filters" class="option-set clearfix" data-option-key="filter">
+			          			<li><a href="#filter" data-option-value="*" class="selected">show all</a></li>
+			          			<li class="heading">Order By</li>
+			          			<li><a href="#filter" data-sort-by="date">Date</a></li>
+			          			<li><a href="#filter" data-sort-by="name">Name</a></li>
+			          			<li class="heading">Show</li>
+			          			<li><a href="#filter" data-option-value=".euphonium">Euphoniums</a></li>
+			          			<li><a href="#filter" data-option-value=".tuba">Tubas</a></li>
+			          			<li class="heading">Recordings</li>
+								<li><a class="recording" href="#filter" data-option-value=".magic">The Tubas Unlimited “Magic Tubas”</a></li>
+								<li><a class="recording" href="#filter" data-option-value=".christmas">Christmas Tubas</a></li>
+								<li><a class="recording" href="#filter" data-option-value=".jazz">TTTE and all that Jazz</a></li>
+								<li><a class="recording" href="#filter" data-option-value=".legacy">Legacy</a></li>
+								<li><a class="recording" href="#filter" data-option-value=".vintage">Vintage</a></li>
+								<li><a class="recording" href="#filter" data-option-value=".heavy">Heavy Metal</a></li>
+								<li><a class="recording" href="#filter" data-option-value=".phat">Phat Bottom Tubas</a></li>
+								<li><a class="recording" href="#filter" data-option-value=".unlimited">Euphoniums Unlimited</a></li>
+								<li><a class="recording" href="#filter" data-option-value=".carnegie">Carnegie VI</a></li>
+								<li><a class="recording" href="#filter" data-option-value=".funky">Play That Funky Tuba Right, Boy</a></li>
+								<li><a class="recording" href="#filter" data-option-value=".beast">Unleash The Beast</a></li>
+								<li><a class="recording" href="#filter" data-option-value=".kings">For the Kings of Brass</a></li>
+								<li><a class="recording" href="#filter" data-option-value=".tubology">Tubology 601</a></li>
+								<li><a class="recording" href="#filter" data-option-value=".garbage">Pierre Garbage Festival</a></li>
+							</ul>
+
+						</div><!--sidebar-options-->
+
+			    	</aside>
+
+			    	<div id="members" class="clearfix">
+
+			      		<article class="member euphonium y1992 y1993">
+			      			<h5>Becky <span class="name">Alexander</span></h5>
+			      			<h6>Euphonium</h6>
+			      			<p class="date">1992-1993</p>
+			      			<a class="btn fancybox fancybox.iframe na">View Profile</a>
+			      		</article>
+
+			      		<article class="member euphonium y1987 y1988">
+			      			<h5>Aaron <span class="name">Angel</span></h5>
+			      			<h6>Euphonium</h6>
+			      			<p class="date">1987-1988</p>
+			      			<a class="btn fancybox fancybox.iframe na">View Profile</a>
+			      		</article>
+
+			      		<article class="member euphonium y2006 y2007 y2008">
+			      			<h5>Michael <span class="name">Altom</span></h5>
+			      			<h6>Euphonium</h6>
+			      			<p class="date">2006-2008</p>
+			      			<a href="/profiles/altom-michael.php" class="btn fancybox fancybox.iframe">View Profile</a>
+			      		</article>
+
+			      		<article class="member euphonium y1978 y1979 y1980 y1981 y1982 y1983">
+			      			<h5>John <span class="name">Arnold</span></h5>
+			      			<h6>Euphonium</h6>
+			      			<p class="date">1978-1983</p>
+			      			<a href="/profiles/arnold-john.php" class="btn fancybox fancybox.iframe">View Profile</a>
+			      		</article>
+
+			      		<article class="member euphonium y1967 y1968">
+			      			<h5>James <span class="name">Baxter</span></h5>
+			      			<h6>Euphonium</h6>
+			      			<p class="date">1967-1968</p>
+			      			<a class="btn fancybox fancybox.iframe na">View Profile</a>
+			      		</article>
+
+			      		<article class="member euphonium y1980">
+			      			<h5>Danette <span class="name">Beasley</span></h5>
+			      			<h6>Euphonium</h6>
+			      			<p class="date">1980-1980</p>
+			      			<a class="btn fancybox fancybox.iframe na">View Profile</a>
+			      		</article>
+
+			      		<article class="member euphonium y1997 y1998 y1999 y2000 y2001 y2002 y2003">
+			      			<h5>Cory <span class="name">Belvin</span></h5>
+			      			<h6>Euphonium</h6>
+			      			<p class="date">1997-2003</p>
+			      			<a href="/profiles/belvin-cory.php" class="btn fancybox fancybox.iframe">View Profile</a>
+			      		</article>
+
+			      		<article class="member euphonium y1991 y1992 y1993 y1994 y1995">
+			      			<h5>Lloyd <span class="name">Bone</span></h5>
+			      			<h6>Euphonium</h6>
+			      			<p class="date">1991-1995</p>
+			      			<a href="/profiles/bone-lloyd.php" class="btn fancybox fancybox.iframe">View Profile</a>
+			      		</article>
+
+			      		<article class="member tuba y1980 y1981 y1982 y1983 y1984 y1985">
+			      			<h5>Charles <span class="name">Abernathy</span></h5>
+			      			<h6>Tuba</h6>
+			      			<p class="date">1980-1985</p>
+			      			<a class="btn fancybox fancybox.iframe na">View Profile</a>
+			      		</article>
+
+			      		<article class="member tuba y1971 y1972 y1973 y1974 y1975 y1976">
+			      			<h5>William <span class="name">Acuff</span></h5>
+			      			<h6>Tuba</h6>
+			      			<p class="date">1971-1976</p>
+			      			<a href="/profiles/acuff-william.php" class="btn fancybox fancybox.iframe">View Profile</a>
+			      		</article>
+
+			      		<article class="member tuba y1989">
+			      			<h5>John <span class="name">Alcock</span></h5>
+			      			<h6>Tuba</h6>
+			      			<p class="date">1989-1989</p>
+			      			<a class="btn fancybox fancybox.iframe na">View Profile</a>
+			      		</article>
+
+			      		<article class="member tuba y1988 y1989 y1990 y1991">
+			      			<h5>Donna <span class="name">Algood</span></h5>
+			      			<h6>Tuba</h6>
+			      			<p class="date">1989-1989</p>
+			      			<a class="btn fancybox fancybox.iframe na">View Profile</a>
+			      		</article>
+
+			      		<article class="member tuba y2005 y2006 y2007 y2008 y2009">
+			      			<h5>Cory <span class="name">Allen</span></h5>
+			      			<h6>Tuba</h6>
+			      			<p class="date">2005-2009</p>
+			      			<a class="btn fancybox fancybox.iframe na">View Profile</a>
+			      		</article>
+
+			      		<article class="member tuba y1973 y1974 y1975 y1976 y1977 y1978">
+			      			<h5>Marcus <span class="name">Arnold</span></h5>
+			      			<h6>Tuba</h6>
+			      			<p class="date">1973-1978</p>
+			      			<a href="/profiles/arnold-marcus.php" class="btn fancybox fancybox.iframe">View Profile</a>
+			      		</article>
+
+			      		<article class="member tuba y1998 y1999 y2000 y2001 y2002">
+			      			<h5>Gray <span class="name">Bach</span></h5>
+			      			<h6>Tuba</h6>
+			      			<p class="date">1998-2002</p>
+			      			<a href="/profiles/bach-gray.php" class="btn fancybox fancybox.iframe">View Profile</a>
+			      		</article>
+
+			      		<article class="member tuba y2009 y2010 y2011">
+			      			<h5>James <span class="name">Baggett</span></h5>
+			      			<h6>Tuba</h6>
+			      			<p class="date">2009-2011</p>
+			      			<a class="btn fancybox fancybox.iframe na">View Profile</a>
+			      		</article>
+
+				    </div><!--members-->
+
+		    	</div><!--container-->
+
+		    </div><!--main-->
+
+	   </div><!--wrapper-->
+
+	</div><!--inner-->
+
+</section><!--main-->
+
+<script>
+
+    $(function(){
+
+      var $container = $('#members');
+
+      $container.isotope({
+        itemSelector : '.member'
+      });
+
+
+      var $optionSets = $('#options .option-set, #sidebar-options .option-set'),
+          $optionLinks = $optionSets.find('a');
+
+      $optionLinks.click(function(){
+        var $this = $(this);
+        // don't proceed if already selected
+        if ( $this.hasClass('selected') ) {
+          return false;
+        }
+        var $optionSet = $this.parents('.option-set');
+        $optionSet.find('.selected').removeClass('selected');
+        $this.addClass('selected');
+
+        // make option object dynamically, i.e. { filter: '.my-filter-class' }
+        var options = {},
+            key = $optionSet.attr('data-option-key'),
+            value = $this.attr('data-option-value');
+        // parse 'false' as false boolean
+        value = value === 'false' ? false : value;
+        options[ key ] = value;
+        if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
+          // changes in layout modes need extra logic
+          changeLayoutMode( $this, options )
+        } else {
+          // otherwise, apply new options
+          $container.isotope( options );
+        }
+
+        return false;
+      });
+
+      $( ".slider" ).slider({
+        value:0,
+        min: 0,
+        max: 48,
+        step: 1,
+        slide: function( event, ui ) {
+          var slideVal = ui.value;
+          $('#filters li:eq('+ slideVal +') a').trigger('click');
+          $('#filters li:eq('+ slideVal +') a').css('color','blue');
+        }
+      });
+
+
+    });
+
+$(".fancybox")
+    .fancybox({
+        padding : 0
+    });
+
+</script>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/footer.php'); // footer, close body and html?>
