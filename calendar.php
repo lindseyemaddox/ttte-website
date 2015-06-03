25ce5d1d03777ecc0667f8577b9b9b40a1933159
lindseyemaddox <lindseyemaddox@gmail.com>
2015-06-03 14:53:09 -0500

    brought back clndr

--- calendar.php
+++ calendar.php
@@ -1,8 +1,12 @@
+@@ -1,4 +1,7 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/head.php'); // HTTP head?>
+<script type="text/javascript" src="/_assets/js/en.js"></script>
+<script type="text/javascript" src="/_assets/js/oCalendar.js"></script>
+<link rel="stylesheet" type="text/css" href="css/blue.css"/>
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/navigation.php'); //navigation?>
 
 <section id="mast">
-	
+
 	<img src="/_assets/img/header-calendar.jpg" alt="R. Winston Morris conducting the Tennessee Tech Tuba Ensemble" title="R. Winston Morris conducting the Tennessee Tech Tuba Ensemble">
 
 </section><!--mast-->
@@ -46,10 +50,9 @@
 		<h1>Calendar of Events</h1>
 
     	<div class="cal1"></div>
+    	<div id="calendar"></div>
 
 		<h6><a href="#">download printable calendar</a> | <a href="#">import calendar to iCal</a> | <a href="#">import calendar to Outlook</a></h6>
-
-		<h5>Events are generally located in Wattenbarger Auditorium in the Bryan Fine Arts Building on the Tennessee Tech Campus in Cookeville, Tennessee. Unless otherwise noted, all events are free, open to the public and everyone is invited. <span>Before traveling any distance confirm event with the tuba studio: <a href="tel:19313723168">(931) 372-3168</a>.</span></h5>
 
 	</div><!--inner-->
 
@@ -70,6 +73,16 @@
     { date: currentMonth + '-' + '23', title: 'Kitten Demonstration', location: 'Center for Beautiful Cats' },
     { date: nextMonth + '-' + '07',    title: 'Small Cat Photo Session', location: 'Center for Cat Photography' }
   ];
+<script>
+$("#calendar").oCalendar({
+    width: 500,
+    height:650,
+    useAbbr:true,
+    startDay:1,
+    allowCud: false,
+    url:"/remote.php"
+}); 
+</script>
 
   $('#full-clndr').clndr({
     template: $('#full-clndr-template').html(),
@@ -81,4 +94,4 @@
  <script src="/_assets/js/site.js"></script>
   <!-- Enable live-reloading in the browser without an extension
   <script src="http://localhost:35729/livereload.js"></script>
-<?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/footer.php'); // footer, close body and html?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/footer.php'); // footer, close body and html?> 
---
+++ remote.php
@@ -0,0 +1,14 @@
+[
+    {
+         "id": "4",
+         "ack": "Meet with the parents",
+         "time": "14:00",
+         "date": "07-05-2015"
+    },
+    {
+         "id": "5",
+         "ack": "Do something :)",
+         "time": "20:00",
+         "date": "13-04-2011"
+    }
+] 
